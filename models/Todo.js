a494e938d5e0eb0b8983a36473787a71f9712e09
$#07 <nonnahs.vaz@gmail.com>
2020-01-08 10:22:50 +0530

    added validation method to Todo model

--- models/Todo.js
+++ models/Todo.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose');
+const Joi = require('joi');
 
 const todoSchema = new mongoose.Schema({
     title: {
         type: String,
+        minlength: 3,
+        maxlength: 255,
         required: true
     }
 });
 
-module.exports = mongoose.model('Todo', todoSchema);
+const validateTodo = (todo) => {
+    const schema = {
+        title: Joi.string().min(3).max(255).required()
+    };
+
+    return Joi.validate(todo, schema);
+}
+
+module.exports.Todo = mongoose.model('Todo', todoSchema);
+module.exports.validateTodo = validateTodo;
