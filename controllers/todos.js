5e7f1e6cfeac202694e5c3109f830628b19cb058
$#07 <nonnahs.vaz@gmail.com>
2020-01-08 10:21:38 +0530

    removed try-catch blocks and added comments

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,106 +1,57 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
+const { Todo, validateTodo } = require('../models/Todo');
 
-const Todo = require('../models/Todo');
-
-// const todos = [
-//     {
-//         id: 1,
-//         title: 'eat'
-//     },
-//     {
-//         id: 2,
-//         title: 'sleep'
-//     },
-//     {
-//         id: 3,
-//         title: 'drink'
-//     }
-// ];
-
+// get all todos from database
 exports.getTodos = async (req, res) => {
-    try {
-        const todos = await Todo.find();
-        return res.send(todos);
-    } catch(error) {
-        console.log(error);
-    }
+    const todos = await Todo.find();
+    return res.send(todos);
 };
 
+// get a single todo from database
 exports.getTodo = async (req, res) => {
-    try {
-        const id = req.params.id;
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-            return res.status(400).send('bad value for id');
-        }
-        const todo = await Todo.findById(id);
-        if (!todo) return res.status(404).send('todo not found');
-        return res.send(todo);
-    } catch (error) {
-        console.log(error);
-    }
+    const id = req.params.id;
+    const todo = await Todo.findById(id);
+    if (!todo) return res.status(404).send('todo not found');
+    return res.send(todo);
 };
 
+// create a new todo
 exports.createTodo = async (req, res) => {
-    try {
-        const todo = req.body;
-        const { error } = validateTodo(todo);
-        if (error) {
-            return res.status(400).send(error.details);
-        } else {
-            const newTodo = new Todo({
-                title: todo.title
-            });
-            const result = await newTodo.save(newTodo);
-            return res.status(201).send(result);
-        }
-    } catch(error) {
-        console.log(error);
+    const todo = req.body;
+    const { error } = validateTodo(todo);
+    if (error) {
+        return res.status(400).send(error.details);
+    } else {
+        const newTodo = new Todo({
+            title: todo.title
+        });
+        const result = await newTodo.save(newTodo);
+        return res.status(201).send(result);
     }
 };
 
+// update existing todo
 exports.updateTodo = async (req, res) => {
-    try {
-        const id = req.params.id;
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-            return res.status(400).send('bad value for id');
-        }
-        const updatedTodo = req.body;
-        const { error } = validateTodo(updatedTodo);
-        if (error) {
-            return res.status(400).send(error.details);
-        }
-        const todo = await Todo.findById(id);
-        if (!todo) {
-            return res.status(404).send('todo not found');
-        }
-        todo.title = updatedTodo.title;
-        return todo.save();
-    } catch(error) {
-        console.log(error);
+    const id = req.params.id;
+    const updatedTodo = req.body;
+    const { error } = validateTodo(updatedTodo);
+    if (error) {
+        return res.status(400).send(error.details);
     }
+    const todo = await Todo.findById(id);
+    if (!todo) {
+        return res.status(404).send('todo not found');
+    }
+    todo.title = updatedTodo.title;
+    const result = await todo.save();
+    return res.send(result);
 };
 
+// delete existing todo
 exports.deleteTodo = async (req, res) => {
-    try {
-        const id = req.params.id;
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-            return res.status(400).send('bad value for id');
-        }
-        const result = await Todo.findByIdAndRemove(id);
-        if (!result) {
-            return res.status(404).send('todo not found');
-        }
-        return res.send(result);
-    } catch(error) {
-        console.log(error);
+    const id = req.params.id;
+    const result = await Todo.findByIdAndRemove(id);
+    if (!result) {
+        return res.status(404).send('todo not found');
     }
+    return res.send(result);
 };
-
-const validateTodo = (todo) => {
-    const schema = {
-        title: Joi.string().min(3).required()
-    };
-
-    return Joi.validate(todo, schema);
-}
